6634282a233a447860197e1d47c57490538fca4b
Sofia20092003 <angelasofiarodriguezarcinegas03@liceolapaz.net>
2025-11-20 10:07:25 +0100

    Ejercicio Calculadora Futbol Terminado

--- src/CalculadoraFutbol/AppLiga.java
+++ src/CalculadoraFutbol/AppLiga.java
@@ -1,7 +1,22 @@
 package CalculadoraFutbol;
 
+import java.util.Scanner;
+
 public class AppLiga {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Escribe el nombre del equipo: ");
+        String nombreEquipo = sc.nextLine();
+        System.out.println("Escribe el numero de victorias del equipo: ");
+        int victorias = sc.nextInt();
+        System.out.println("Escribe el numero de empates del equipo: ");
+        int empates = sc.nextInt();
+        CalculadoraLiga.ObtenerMensajeAficion(nombreEquipo);
+        int resultados=CalculadoraLiga.calcularpuntos(victorias, empates);
+        if(CalculadoraLiga.esDescenso(resultados)){
+            System.out.println(nombreEquipo+ " ha descendido.");
+        }else {
+            System.out.println(nombreEquipo+ " ha ascendido.");
+        }
     }
 }
