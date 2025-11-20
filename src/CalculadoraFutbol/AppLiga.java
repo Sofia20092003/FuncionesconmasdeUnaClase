8a51c7ec354e6aba2a616519da5a93834c0f682a
Sofia20092003 <angelasofiarodriguezarcinegas03@liceolapaz.net>
2025-11-20 09:43:37 +0100

    Iniciando Ejercicios de Funciones de mas de una clase

---
+++ src/CalculadoraFutbol/AppLiga.java
@@ -0,0 +1,7 @@
+package CalculadoraFutbol;
+
+public class AppLiga {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/CalculadoraFutbol/CalculadoraLiga.java
@@ -0,0 +1,4 @@
+package CalculadoraFutbol;
+
+public class CalculadoraLiga {
+}
